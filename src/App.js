0481726638a08a10ffc01a9e0bf682e24fbcfdce
Son Ta <sontavac@gmail.com>
2022-03-21 23:48:55 +0700

    Lab03.1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './component/MenuComponent';
 import './App.css';
 class App extends Component {
 
   render() {
     return (
-      <Navbar dark color="primary">
+      <div className='App'>
+        <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
-      </Navbar>
+        </Navbar>
+        <Menu />
+      </div>
+      
     );
   }
 }
