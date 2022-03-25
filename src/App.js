866a2712b137f9c926774f79ed6b3837a855547e
Son Ta <sontavac@gmail.com>
2022-03-25 21:41:51 +0700

    LAB04-01

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './component/MenuComponent';
-import { DISHES } from './shared/dishes';
+import Main from './component/MainComponent';
 import './App.css';
-import DishDetail from './component/DishdetailComponent';
+
+
 class App extends Component {
 
 
-// Khai báo trạng thái lên App.js
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dishes: DISHES
-    };
-  }
-
   render() {
     return (
-      <div className='App'>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-
-        {/* Truyền trạng thái của App.js cho thằng con Menu */}
-        <Menu dishes={this.state.dishes}/>
-        <DishDetail dishes={this.state.dishes}/>
+      <div>
+        <Main/>
       </div>
-      
     );
   }
 }
