366efbe02aa06d457fe1d4a7ae3e5d92bb38ad5b
Son Ta <sontavac@gmail.com>
2022-03-30 20:46:05 +0700

    First Step

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import { STAFFS } from './shared/staffs';
-import StaffList from './component/StaffListComponent'
+import Main from './component/MainComponent';
+import { BrowserRouter } from 'react-router-dom'
 import './App.css';
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      staffs: STAFFS
-    };
-  }
-
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-          </div>
-        </Navbar>
-
-        <StaffList staffs={this.state.staffs} />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
