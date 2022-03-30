44184fb5d3db2e4009cab81749b3a5f734d42941
Son Ta <sontavac@gmail.com>
2022-03-30 21:35:02 +0700

    Footer Component

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Footer from "./Footer";
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 
 class Main extends Component{
     render(){
         return(
-            <Header />
+            <div>
+                <Header />
+                <Footer />
+            </div>
         );
     }
 }
