27964c2f95459bdb052d0b7cb85a4418f412c7e7
Son Ta <sontavac@gmail.com>
2022-03-31 20:43:14 +0700

    StaffDetail Component part2

--- src/component/Staff.js
+++ src/component/Staff.js
@@ -1,37 +1,38 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardBody, CardSubtitle } from "reactstrap";
+import { Card, CardImg, CardBody, CardSubtitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from 'react-router-dom'
 
-class StaffList extends Component{
-    constructor(props){
-        super(props)
-        console.log(props.staffs)
-    }
-
-    render() {
-        const RenderStaffList = this.props.staffs.map((staff) => {
-            return(
-                <div className="col-6 col-md-4 col-lg-2 mt-3 mb-3">
-                    <Card>
+function StaffList(props) {
+    const RenderStaffList = props.staffs.map((staff) => {
+        return(
+            <div className="col-6 col-md-4 col-lg-2 mt-3 mb-3">
+                <Card>
+                    <Link to={`/staffs/${staff.id}`}>
                         <CardImg width="100%" src={staff.image} alt={staff.name} />
                         <CardBody>
                             <CardSubtitle>{staff.name}</CardSubtitle>
                         </CardBody>
-                    </Card>
-                </div>
-            );
-        })
-
-        return(
-            <div className="container">
-                <div className="row">
-                    <h3>Nhân Viên</h3>
-                </div>
-                <div className="row">
-                    {RenderStaffList}
-                </div>
+                    </Link>
+                </Card>
             </div>
         );
-    }
+    })
+
+    return(
+        <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/staffs'>Nhân viên</Link></BreadcrumbItem>
+                </Breadcrumb>
+            </div>
+            <div className="row">
+                <h3>Nhân Viên</h3>
+            </div>
+            <div className="row">
+                {RenderStaffList}
+            </div>
+        </div>
+    );
 }
 
 export default StaffList
