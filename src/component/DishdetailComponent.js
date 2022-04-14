f0f2b7383544326849d691213e181df2175e20d2
Son Ta <sontavac@gmail.com>
2022-04-14 23:51:23 +0700

    Lab10.04

--- src/component/DishdetailComponent.js
+++ src/component/DishdetailComponent.js
@@ -30,7 +30,7 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     this.toggleCommentForm();
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
   }
 
 
@@ -120,7 +120,7 @@ class CommentForm extends Component {
     );
   }
 
-  function RenderComments({comments, addComment, dishId}) {
+  function RenderComments({comments, postComment, dishId}) {
     if (comments != null) {
       return (
         <div className="col-12 col-md-5 m-1">
@@ -138,7 +138,7 @@ class CommentForm extends Component {
               );
             })}
           </ul>
-          <CommentForm dishId={dishId} addComment={addComment} /> 
+          <CommentForm dishId={dishId} postComment={postComment} /> 
         </div>
       );
     } else {
@@ -182,7 +182,7 @@ class CommentForm extends Component {
           <div className="row">
             <RenderDish dish={props.dish} />
             <RenderComments comments={props.comments}
-              addComment={props.addComment}
+              postComment={props.postComment}
               dishId={props.dish.id}
             />
             {/* <CommentForm /> */}
